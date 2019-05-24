585504a8ac16a19c5f79d8cac40931efd06404ed
Vladislav Kudrin <vladislavkudrin@gmail.com>
2019-05-24 19:34:12 +0200

    Dont

--- live-static-files/static-root/js/ecommerce.js
+++ live-static-files/static-root/js/ecommerce.js
@@ -1,5 +1,89 @@
+//Autofill Search 
+var searchForm = $(".search-form")
+var searchInput = searchForm.find("[name='q']") //input name = 'q'
+var actionEndpoint = searchForm.attr("action");
+//var typingTimer;
+// var typingInterval = 50 //0.5 seconds
+//var searchBtn = searchForm.find("[type='submit']")
+// // define displaySearching function
+// function displaySearching(){
+// searchBtn.addClass("disabled")
+// searchBtn.html("<i class='fas fa-spin fa-spinner'></i> Searching....")
+//         }
 $(document).ready(
+      (function() {
+      $.ajax({
+      url: actionEndpoint,
+      data: searchInput,
+      success: function(data){
+              var availableTags = data.filtered_products
+              var searchBtn = searchForm.find("[type='submit']")
+              $( "#searchAutoComplete" ).autocomplete({
+                          source: availableTags
+                          }).data("ui-autocomplete")._renderItem=function (ul, item) { //for  clicking results
+                          return $("<li></li>")
+                          .data("item.autocomplete", item)
+                          .append("<a href='/search/?q=" + item.value + "'>"+"<span class='suggestions'>" +item.value+ "</span></a>")
+                          .appendTo(ul);
+                          };
+              }, 
+      error: function(errorData){
+          $.alert({
+              title: 'OOps!',
+              content: 'Simple alert!',
+              theme: "modern"
+            });
+            }
+      })//ajax
+      }
+      ))//docreary
+
+      
+// //Auto Search
+// var searchForm = $(".search-form")
+// var searchInput = searchForm.find("[name='q']") //input name = 'q'
+// var typingTimer;
+// var typingInterval = 500 //0.5 seconds
+// var searchBtn = searchForm.find("[type='submit']")
+// // console.log(searchBtn)
+// searchInput.keyup(
+//   function(event){
+//     clearTimeout(typingTimer)
+//     typingTimer = setTimeout(performSearch, typingInterval)
+//   })
+//   searchInput.keydown(
+//  //key pressed
+//   function(event){
+//     clearTimeout(typingTimer)
+ 
+//   })
+  
+//   function displaySearching(){
+//     searchBtn.addClass("disabled")
+//     searchBtn.html("<i class='fas fa-spin fa-spinner'></i> Searching....")
+//   }
+
+//   function performSearch(){
+//     displaySearching()
+//     var query = searchInput.val()
+//     setTimeout(function(){
+//       window.location.href='/search/?q=' + query   
+//     }, 1000)
+               
+//   }
+
+
+
+
+
+
+
+$(document).ready(
+
+
   function(){
+  
+
     //contactFormHandler
     var contactForm = $('.contact-form')
     var contactFormMethod = contactForm.attr("method")
@@ -11,7 +95,7 @@ $(document).ready(
       } else {
         submitBtn.removeClass("disabled")
         submitBtn.html(defaultText)
-      }
+        }
     }            
     contactForm.submit(
       function(event){
@@ -31,7 +115,7 @@ $(document).ready(
               title: 'Success',
               content: data.message,
               theme: "modern"
-          })
+              })
           setTimeout(
             function()
             {displaySubmitting(contactFormSubmitBtn, contactFormSubmitBtnTxt,false)}, 1000)
@@ -54,7 +138,7 @@ $(document).ready(
             {displaySubmitting(contactFormSubmitBtn, contactFormSubmitBtnTxt,false)}, 1000)
           }
         })
-      })
+      });
 
 
 
@@ -65,43 +149,16 @@ $(document).ready(
 
 
 
+   
 
 
 
-    //Auto Search
-    var searchForm = $(".search-form")
-    var searchInput = searchForm.find("[name='q']") //input name = 'q'
-    var typingTimer;
-    var typingInterval = 500 //0.5 seconds
-    var searchBtn = searchForm.find("[type='submit']")
-    searchInput.keyup(  //released key
-      function(event){
-        clearTimeout(typingTimer)
-        typingTimer = setTimeout(performSearch, typingInterval)
-      })
-    searchInput.keydown( //key pressed
-      function(event){
-        clearTimeout(typingTimer)
-     
-      })
-      
-      function displaySearching(){
-        searchBtn.addClass("disabled")
-        searchBtn.html("<i class='fas fa-spin fa-spinner'></i> Searching....")
-      }
 
-      function performSearch(){
-        displaySearching()
-        var query = searchInput.val()
-        setTimeout(function(){
-          window.location.href='/search/?q=' + query   
-        }, 1000)
-                   
-      }
+
+
 
 
   
-
 
   //Product Delete Alert Ajax
 
@@ -287,7 +344,7 @@ $(document).ready(
 
 
 
-// Wishlist
+// Wishlist Product-List-View
 
     var productForm=$(".form-product-ajax-wishlist")
     productForm.submit(
@@ -306,39 +363,86 @@ $(document).ready(
           method: httpMethod,
           data: formData,
           success: function(data){
+            
+            var submitSpan = thisForm.find(".submit-span-wishlist")
+            if (data.added){
+              submitSpan.html("<button type='submit' class='hidden-button hidden-button-outline'><i class='fas fa-heart fa-5x'></i></button>")
+            }
+            else {
+              submitSpan.html("<button type='submit' class='hidden-button hidden-button-outline'><i class='far fa-heart fa-5x'></i></button>")
+            }
+
+            
+            var navbarCount = $(".navbar-wish-count")
+            navbarCount.text(data.wishes_count) // текст с навбар коунт заменяется отправленым нами wishes_count
+
+          },
+          error: function(errorData){
+            $.alert({
+              title: 'Oops!',
+              content: errorData,
+              theme: "modern",
+
+            });
+            }
+        })
+
+     }
+
+    )
+   
+
+// Wishlist Product-Detail-View 
+
+    var productFormDetail=$(".form-product-ajax-wishlist-detail")
+    productFormDetail.submit(
+
+      function(event){
+
+        event.preventDefault()
+        var thisForm = $(this)
+        var actionEndpoint = thisForm.attr("action");
+        var actionEndpoint = thisForm.attr("data-endpoint");
+        var httpMethod = thisForm.attr("method");
+        var formData = thisForm.serialize();
+
+        $.ajax({
+          url: actionEndpoint,
+          method: httpMethod,
+          data: formData,
+          success: function(data){
+            
             var submitSpan = thisForm.find(".submit-span-wishlist")
             if (data.added){
               submitSpan.html("<button type='submit' class='hidden-button hidden-button-outline'><i class='fas fa-heart fa-2x'></i></button>")
             }
             else {
               submitSpan.html("<button type='submit' class='hidden-button hidden-button-outline'><i class='far fa-heart fa-2x'></i></button>")
-            }},
-        
+            }
+
+            
+            var navbarCount = $(".navbar-wish-count")
+            navbarCount.text(data.wishes_count) // текст с навбар коунт заменяется отправленым нами wishes_count
+
+          },
           error: function(errorData){
             $.alert({
               title: 'Oops!',
-              content: "Apparently you haven't signed up yet...",
-              theme: "modern"
+              content: errorData,
+              theme: "modern",
+
             });
-  }
-})
+            }
+        })
 
+     }
 
-
-
-
-      }
-
-
-
-
-
-    )
-      
+    )  
     
 
-  //   function refreshCart(){
-  //     console.log("privet")
+    // function refreshCart(){
+    //   console.log("privet")
+    // }
   //     var cartTable = $(".cart-table")
   //     var cartBody = cartTable.find(".cart-body")
   //     //cartBody.html("<h1>Changed</h1>")
