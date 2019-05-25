142783574306189db918166886c6d8ad556e33fe
Vladislav Kudrin <vladislavkudrin@gmail.com>
2019-05-25 12:37:13 +0200

    Update forms.py

--- products/forms.py
+++ products/forms.py
@@ -26,9 +26,7 @@ class ProductCreateForm(FileFormMixin, forms.ModelForm):
 			]
 	def __init__(self, request, *args, **kwargs):
 		self.request = request
-		super(ProductCreateForm, self).__init__(*args, **kwargs)	
-		brand = Brand.objects.get(id=self.initial['brand'])
-		self.initial['brand']=brand.brand_name
+		super(ProductCreateForm, self).__init__(*args, **kwargs)
 
 	def clean_category(self):
 		request = self.request
@@ -89,6 +87,10 @@ class ProductUpdateForm(ProductCreateForm):
 		category = product.category
 		size = Size.objects.filter(size_for__icontains=category)
 		self.fields['size'].queryset = size
+		brand = Brand.objects.get(id=self.initial['brand'])
+		if brand.exists():
+				self.initial['brand']=brand.brand_name
+
 		def get_upload_url(self):
 			return reverse('products:example_handle_upload')
 
