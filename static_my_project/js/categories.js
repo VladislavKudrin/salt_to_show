e1b0bd93a25e5344cfcd346db6739f2e2fd8be40
roma <romhedoo@gmail.com>
2019-07-03 23:46:47 +0200

    Last try

--- static_my_project/js/categories.js
+++ static_my_project/js/categories.js
@@ -306,13 +306,13 @@ $("#slider").slideReveal({
 //slider filter box
 
 $("#slider_2").slideReveal({
-    console.log('fdfd')
   trigger: $("#trigger_2"),
   push: false,
   overlay: true,
   overlayColor:'rgba(0,0,0,0.5)',
   width:'500px',
 });
+
 
 
 // Add slideDown animation to Bootstrap dropdown when expanding.
