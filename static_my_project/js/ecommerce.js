e5187a042b6a1aa97558286b15585098a7effff8
roma <romhedoo@gmail.com>
2019-07-04 10:08:33 +0200

    ebateka

--- static_my_project/js/ecommerce.js
+++ static_my_project/js/ecommerce.js
@@ -34,13 +34,13 @@ $(document).ready(
 })
 
       
-$("#slider_2").slideReveal({
-  trigger: $("#trigger_2"),
-  push: false,
-  overlay: true,
-  overlayColor:'rgba(0,0,0,0.5)',
-  width:'500px',
-});
+// $("#slider_2").slideReveal({
+//   trigger: $("#trigger_2"),
+//   push: false,
+//   overlay: true,
+//   overlayColor:'rgba(0,0,0,0.5)',
+//   width:'500px',
+// });
 
 
 
