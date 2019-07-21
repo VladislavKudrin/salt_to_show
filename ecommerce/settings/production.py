663c2ee33222a5595492ae1b4073220779c87d86
roma <romhedoo@gmail.com>
2019-07-21 15:46:11 +0200

    'SALT <info@saltish.co>'

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -44,7 +44,7 @@ EMAIL_HOST_USER = 'info@saltish.co'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'SALT Admin'
+DEFAULT_FROM_EMAIL = 'SALT <info@saltish.co>'
 BASE_URL = 'https://www.saltish.co'
 
 MANAGERS = (
