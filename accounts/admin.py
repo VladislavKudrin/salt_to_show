823ed7a5fe4fe94c12bf641e27225e55210c5383
Vladislav Kudrin <vladislavkudrin@gmail.com>
2019-09-25 12:43:11 +0200

    User timestamp admin view

--- accounts/admin.py
+++ accounts/admin.py
@@ -19,7 +19,7 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'admin')
+    list_display = ('email', 'admin', 'timestamp')
     list_filter = ('admin', 'staff', 'is_active')
     fieldsets = (
         (None, {'fields': ('email', 'password', 'username')}),
