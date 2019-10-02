e58c9449041f1bc3a137e1da0a69ce2b6094f46a
Vladislav Kudrin <vladislavkudrin@gmail.com>
2019-10-02 10:34:52 +0200

    Chat_orderFix

--- chat_ecommerce/views.py
+++ chat_ecommerce/views.py
@@ -70,7 +70,7 @@ class ThreadView(LoginRequiredMixin, FormMixin, DetailView):
         threads_with_unred = Thread.objects.filter(chatmessage__notification__user=me, chatmessage__notification__read='False').distinct() # all threads with notifications where this specific request.user received notification! # filter by foreign key with multiple filters
         context['threads_with_unred'] = threads_with_unred
         context['form'] = self.get_form()
-
+        context['chat_messages_ordered'] = obj.chatmessage_set.all().order_by('pk')
         context['chats'] = Thread.objects.by_recent_message(self.request.user)
         # print(context['chats'])
          # = Thread.objects.filter(chatmessage__user__notification=1)
