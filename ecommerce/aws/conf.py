a33c5538e6aad7a6b64baf90326296e88a223b72
Vladislav Kudrin <vladislavkudrin@gmail.com>
2019-10-15 14:11:31 +0200

    Update conf.py

--- ecommerce/aws/conf.py
+++ ecommerce/aws/conf.py
@@ -8,10 +8,10 @@ AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
 AWS_QUERYSTRING_AUTH = True
 
-AWS_S3_REGION_NAME = 'eu-central-1'
+AWS_S3_REGION_NAME = 'us-east-1'
 DEFAULT_FILE_STORAGE = 'ecommerce.aws.utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'ecommerce.aws.utils.StaticRootS3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'salt-staging'
+AWS_STORAGE_BUCKET_NAME = 'salt-staging-usa'
 S3_URL = '//%s.s3.amazonaws.com/' % (AWS_STORAGE_BUCKET_NAME)
 MEDIA_URL = '//%s.s3.amazonaws.com/media/' % (AWS_STORAGE_BUCKET_NAME)
 MEDIA_ROOT = MEDIA_URL
@@ -26,3 +26,35 @@ AWS_HEADERS = {
     'Expires': expires,
     'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
 }
+
+
+
+
+# import datetime
+
+# AWS_GROUP_NAME = 'Salt-Staging'
+# AWS_USERNAME = 'salt-staging'
+# AWS_ACCESS_KEY_ID = "AKIA2R5PIZK6GSBLECBR"
+# AWS_SECRET_ACCESS_KEY = "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"
+# AWS_FILE_EXPIRE = 200
+# AWS_PRELOAD_METADATA = True
+# AWS_QUERYSTRING_AUTH = True
+
+# AWS_S3_REGION_NAME = 'eu-central-1'
+# DEFAULT_FILE_STORAGE = 'ecommerce.aws.utils.MediaRootS3BotoStorage'
+# STATICFILES_STORAGE = 'ecommerce.aws.utils.StaticRootS3BotoStorage'
+# AWS_STORAGE_BUCKET_NAME = 'salt-staging'
+# S3_URL = '//%s.s3.amazonaws.com/' % (AWS_STORAGE_BUCKET_NAME)
+# MEDIA_URL = '//%s.s3.amazonaws.com/media/' % (AWS_STORAGE_BUCKET_NAME)
+# MEDIA_ROOT = MEDIA_URL
+# STATIC_URL = S3_URL + 'static/'
+# ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+# two_months = datetime.timedelta(days=61)
+# date_two_months_later = datetime.date.today() + two_months
+# expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+
+# AWS_HEADERS = { 
+#     'Expires': expires,
+#     'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+# }
