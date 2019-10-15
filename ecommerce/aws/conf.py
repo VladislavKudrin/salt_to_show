63fb864f545a0a1c82866cb8907f3dba9709111a
Vladislav Kudrin <vladislavkudrin@gmail.com>
2019-10-15 14:44:11 +0200

    Update conf.py

--- ecommerce/aws/conf.py
+++ ecommerce/aws/conf.py
@@ -1,19 +1,16 @@
 import datetime
-
-AWS_GROUP_NAME = 'Salt-Staging'
-AWS_USERNAME = 'salt-staging'
 AWS_ACCESS_KEY_ID = "AKIA2R5PIZK6GSBLECBR"
 AWS_SECRET_ACCESS_KEY = "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"
 AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
-AWS_QUERYSTRING_AUTH = True
 
-AWS_S3_REGION_NAME = 'us-east-2'
+
 DEFAULT_FILE_STORAGE = 'ecommerce.aws.utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'ecommerce.aws.utils.StaticRootS3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'salt-staging-usa'
-S3_URL = '//%s.s3.amazonaws.com/' % (AWS_STORAGE_BUCKET_NAME)
-MEDIA_URL = '//%s.s3.amazonaws.com/media/' % (AWS_STORAGE_BUCKET_NAME)
+S3DIRECT_REGION = 'eu-central-1'
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_ROOT = MEDIA_URL
 STATIC_URL = S3_URL + 'static/'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
@@ -26,9 +23,7 @@ AWS_HEADERS = {
     'Expires': expires,
     'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
 }
-
-
-
+AWS_QUERYSTRING_AUTH = False
 
 # import datetime
 
