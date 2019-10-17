d9c19d45ecbe77586b1fe53d06805d3b8683b1dd
Vladislav Kudrin <vladislavkudrin@gmail.com>
2019-10-17 12:58:23 +0200

    migrate eu server

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
     
-ALLOWED_HOSTS = ['.saltish.co', 'envision-outfit.herokuapp.com']
+ALLOWED_HOSTS = ['.saltish.co', 'salt-eu.herokuapp.com']
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'info@saltish.co' 
