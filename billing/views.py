3b027cd058fe815f95a479994ab10107ae51b010
Vladislav Kudrin <vladislavkudrin@gmail.com>
2020-03-12 13:28:49 +0100

    Translations and Url final fix

--- billing/views.py
+++ billing/views.py
@@ -32,7 +32,7 @@ class PayView(TemplateView):
     template_name = 'billing/pay.html'
     def get(self, request, *args, **kwargs):
         liqpay = LiqPay(LIQPAY_PUB_KEY, LIQPAY_PRIV_KEY)
-        callback_url = settings.HEROKU_TEST_URL + reverse('payment:pay_callback')
+        callback_url = settings.BASE_URL_WITHOUT_WWW + reverse('payment:pay_callback')
         if settings.TESTMODE:
             callback_url = "https://enbarnzipr23p.x.pipedream.net"
         order = Order.objects.by_request(request).first()
@@ -58,13 +58,6 @@ class PayView(TemplateView):
 @method_decorator(csrf_exempt, name='dispatch')
 class PayCallbackView(View):
     def post(self, request, *args, **kwargs):
-        print('PRIVET!!!!!!!!')
-        print('PRIVET!!!!!!!!')
-        print('PRIVET!!!!!!!!')
-        print('PRIVET!!!!!!!!')
-        print('PRIVET!!!!!!!!')
-        print('PRIVET!!!!!!!!')
-        print('PRIVET!!!!!!!!')
         liqpay = LiqPay(LIQPAY_PUB_KEY, LIQPAY_PRIV_KEY)
         data = request.POST.get('data')
         signature = request.POST.get('signature')
--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -50,7 +50,6 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'SALT <info@saltish.co>'
 
 
-HEROKU_TEST_URL = os.environ.get('HEROKU_TEST_URL')
 
 if TESTSERVER == 'True':
     BASE_URL = 'https://www.salt-testserver.herokuapp.com'
@@ -60,7 +59,7 @@ if TESTSERVER == 'True':
     ALLOWED_HOSTS = ['salt-testserver.herokuapp.com']
 else:
     BASE_URL = 'https://www.saltish.co'
-    BASE_URL_WITHOUT_WWW = 'https://saltish.co'
+    BASE_URL_WITHOUT_WWW = 'https://www.saltish.co'
     LIQPAY_PUBLIC_KEY = os.environ.get('LIQPAY_PUBLIC_KEY')
     LIQPAY_PRIVATE_KEY = os.environ.get('LIQPAY_PRIVATE_KEY')
     ALLOWED_HOSTS = ['.saltish.co', 'salt-eu.herokuapp.com']
