787e59e13a99a1f2b56e57a757b88e19b06acfa4
roma <romhedoo@gmail.com>
2020-03-27 17:51:39 +0100

    aws fixed

--- ecommerce/aws/conf.py
+++ ecommerce/aws/conf.py
@@ -23,9 +23,9 @@ two_months = datetime.timedelta(days=61)
 date_two_months_later = datetime.date.today() + two_months
 expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
 
-AWS_HEADERS = { 
+AWS_S3_OBJECT_PARAMETERS = { 
     'Expires': expires,
-    'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+    'CacheControl': 'max-age=%d' % (int(two_months.total_seconds()), ),
 }
 AWS_QUERYSTRING_AUTH = False
 AWS_IS_GZIPPED = True
