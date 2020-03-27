dd80653a5119fe2bfa5b37f36ac6ef6ac3a8ee7f
roma <romhedoo@gmail.com>
2020-03-27 16:23:05 +0100

    Test

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -180,7 +180,7 @@ AUTHENTICATION_BACKENDS = [
         ]
 
 MIDDLEWARE = [
-    'django.middleware.gzip.GZipMiddleware',
+    'compression_middleware.middleware.CompressionMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
