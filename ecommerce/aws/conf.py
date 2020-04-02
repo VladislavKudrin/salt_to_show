8c86f00fb384b54f4b65f24c71c6c78126c3b64c
Vladislav Kudrin <vladislavkudrin@gmail.com>
2020-04-02 12:26:16 +0200

    Update conf.py

--- ecommerce/aws/conf.py
+++ ecommerce/aws/conf.py
@@ -1,20 +1,26 @@
 import datetime
 from django.conf import settings
 
-AWS_ACCESS_KEY_ID = "AKIA2R5PIZK6M3HVE26S"
-AWS_SECRET_ACCESS_KEY = "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"
+
 AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
 
-AWS_GROUP_NAME = 'EnvisionGroup'
-AWS_USERNAME = 'envision-user'
+
 
 DEFAULT_FILE_STORAGE = 'ecommerce.aws.utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'ecommerce.aws.utils.StaticRootS3BotoStorage'
 if settings.TESTSERVER == 'True':
 	AWS_STORAGE_BUCKET_NAME = 'salt-staging'
+	AWS_ACCESS_KEY_ID = "AKIA2R5PIZK6FDQ5PRUR"
+	AWS_SECRET_ACCESS_KEY = "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"
+	AWS_GROUP_NAME = 'Salt-Staging'
+	AWS_USERNAME = 'salt-staging'
 else:
 	AWS_STORAGE_BUCKET_NAME = 'salt-bucket-eu'
+	AWS_ACCESS_KEY_ID = "AKIA2R5PIZK6M3HVE26S"
+	AWS_SECRET_ACCESS_KEY = "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"
+	AWS_GROUP_NAME = 'EnvisionGroup'
+	AWS_USERNAME = 'envision-user'
 
 S3DIRECT_REGION = 'eu-central-1'
 S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
@@ -37,34 +43,3 @@ AWS_IS_GZIPPED = True
 
 
 
-# ----- TESTSERVER -----
-
-
-# import datetime
-
-# AWS_GROUP_NAME = 'Salt-Staging'
-# AWS_USERNAME = 'salt-staging'
-# AWS_ACCESS_KEY_ID = "AKIA2R5PIZK6GSBLECBR"
-# AWS_SECRET_ACCESS_KEY = "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"
-# AWS_FILE_EXPIRE = 200
-# AWS_PRELOAD_METADATA = True
-# AWS_QUERYSTRING_AUTH = True
-
-# AWS_S3_REGION_NAME = 'eu-central-1'
-# DEFAULT_FILE_STORAGE = 'ecommerce.aws.utils.MediaRootS3BotoStorage'
-# STATICFILES_STORAGE = 'ecommerce.aws.utils.StaticRootS3BotoStorage'
-# AWS_STORAGE_BUCKET_NAME = 'salt-staging'
-# S3_URL = '//%s.s3.amazonaws.com/' % (AWS_STORAGE_BUCKET_NAME)
-# MEDIA_URL = '//%s.s3.amazonaws.com/media/' % (AWS_STORAGE_BUCKET_NAME)
-# MEDIA_ROOT = MEDIA_URL
-# STATIC_URL = S3_URL + 'static/'
-# ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-
-# two_months = datetime.timedelta(days=61)
-# date_two_months_later = datetime.date.today() + two_months
-# expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
-
-# AWS_HEADERS = { 
-#     'Expires': expires,
-#     'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
-# }
