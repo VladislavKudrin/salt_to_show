73947e2f19ac97390a0ff02e57911b15bdea4847
roma <romhedoo@gmail.com>
2020-06-03 10:29:13 +0200

    Update views.py

--- bot/views.py
+++ bot/views.py
@@ -267,7 +267,6 @@ def send_message_to_channel(product):
 """"""🧂<b>Price:</b> <i>"""+str(product.price_original)+' '+product.currency_original+"""</i>
 """"""<b>.</b>
 """"""<b>.</b>
-""""""<b><ins><a href=""""+product.get_absolute_url+"""">SEE ON SALT</a></ins></b>
 """"""<b><ins><a href="https://t.me/saltish_bot?start="""+product.slug+"""">BUY</a></ins></b>"""
 				media_type=types.InputMediaPhoto(media=image.image, caption=text, parse_mode='HTML')
 				media_types.append(media_type)
