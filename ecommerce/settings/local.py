4451953bc091b59ec1df4ef233d7e7ac6530e32f
roma <romhedoo@gmail.com>
2020-06-03 11:52:12 +0200

    Small modifications on Channel

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -52,7 +52,7 @@ EMAIL_HOST_PASSWORD = 'Ramankondrotiev123'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'SALT <info@saltish.co>'
-BASE_URL = 'https://5c6581b1.ngrok.io'
+BASE_URL = 'https://ae1383133c28.ngrok.io'
 BASE_URL_WITHOUT_WWW = '127.0.0.1:8000'
 
 REST_FRAMEWORK = {
