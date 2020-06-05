14c06e5b467e47b97d0ed251c37801cd70b9f1ed
Vladislav Kudrin <vladislavkudrin@gmail.com>
2020-06-05 16:37:52 +0200

    Add delete Key request

--- bot/urls.py
+++ bot/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import BotView
+from .views import BotView, delete_activation_key_view
 
 urlpatterns = [
 	url(r'^telegram/$', BotView.as_view(), name='telegram_bot'),
+	url(r'^delete_activation/$', delete_activation_key_view, name='delete_activation_key'),
 ]
