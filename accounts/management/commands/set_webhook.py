f5f0fe90d9cd27728d85a47039f195440a68413e
Vladislav Kudrin <vladislavkudrin@gmail.com>
2020-06-26 11:15:22 +0200

    Update set_webhook.py

--- accounts/management/commands/set_webhook.py
+++ accounts/management/commands/set_webhook.py
@@ -8,6 +8,6 @@ class Command(BaseCommand):
     help = 'Set webhook for local'
 
     def handle(self, *args, **kwargs):
-    	if not settings.LIVE and not settings.TESTSERVER:
+    	if not settings.LIVE:
     		t = requests.get('https://api.telegram.org/bot1261478236:AAGtaxf4gqGf562PIcCXmkK7cHGjVQKNf5M/setWebhook?url='+settings.BASE_URL+'/api/telegram/')
     		print(t.text)
